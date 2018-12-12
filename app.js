a06e860651f1d23c288cd7acef1bc09ad6d072c0
heshko <shooter.metall@gmail.com>
2018-12-12 13:23:33 +0100

    first

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-
-var app = angular.module("myApp",["ngRoute"])
+var app =angular.module("FirebaseApp",["ngRoute","firebase"])
 app.config(["$routeProvider",function($routeProvider){
-$routeProvider
-.when("/home",{
-  templateUrl:"item1.html",
-  controller:"control1"
-})
-.when("/antoon",{
-  templateUrl:"item2.html",
-  controller:"control2"
-})
-
+    $routeProvider
+    .when("/",{
+        templateUrl:"/templates/home.html",
+        controller:"firstController"
+    })
+    .when("/sign-up",{
+        templateUrl:"/templates/sign-up.html",
+        controller:"SignUpController"
+    })
+    .when("/login",{
+        templateUrl:"/templates/login.html",
+        controller:"loginController"
+    })
+    .when("/translate",{
+        templateUrl:"translate.html",
+        controller:"control1"
+    })
+    
+    
 }])
--- controller.js
+++ controller.js
@@ -1,48 +1,105 @@
-var app =angular.module("myApp")
-app.controller("control1",function(){
-    function speak() {
-        var x = document.getElementById("t")
-        var text = x.value;
-        var read = new SpeechSynthesisUtterance(text)
-        read.lang = document.getElementById("språk")
-        window.speechSynthesis.speak(read)
-      }
-      
-      function startRecording() {
-      
-        if (window.hasOwnProperty('webkitSpeechRecognition')) {
-      
-          var recognition = new webkitSpeechRecognition();
-          recognition.continuous = false; // för att recording stop om 10 sek // om det är 
-          recognition.interimResults = true;// om det är true så det skriva som jag säger 
-      
-          recognition.lang = document.getElementById("chang-språk-1").value;// jag gjorde det för att byta språk 
-          recognition.start(); // för att börjar rccording
-      
-          recognition.onresult = function (e) {
-            document.getElementById('transcript').value
-              = e.results[0][0].transcript;
-          };
+var app = angular.module("FirebaseApp");
+app.controller("firstController", ["$scope","$firebaseAuth", function ($scope, $firebaseAuth) {
+    console.log($firebaseAuth())// firebaseAuth det här method låta mig att göra mång function som log in och signout och signUp
+    $firebaseAuth().$onAuthStateChanged(function(user){ //الطريقة الموصى بها للحصول على المستخدم الحالي هي عن طريق تعيين مراقب على كائن المصادقة:
+console.log(user)
+if(user){
+    $scope.user= user;
+}else{
+    $scope.user= null;
+}
+    })
+}]);
+app.controller("SignUpController", ["$scope", "$firebaseAuth", "$location", function ($scope, $firebaseAuth, $location) {
+
+    $scope.signUp = function (user) { // det är en function som jag skriv denna i form ng-submit="singUp(user)"
+        $firebaseAuth().$createUserWithEmailAndPassword(user.email, user.password) // de som jag skriver inner button som ng-modle
+            .then(function (fireUser) {
+                if (fireUser) {
+                    var theUser = firebase.auth().currentUser;//????
+
+                    theUser.updateProfile({
+                        displayName: user.username 
+                    }).then(function () {
+                        
+                    }).catch(function (error) {
+                       console.log(error)
+                        
+                        
+                    });
+                    firebase.database().ref('users/' + fireUser.id).set({
+                        username: user.username
+                    });
+
+                    $location.path('/');
+                    
+                }
+
+            })
+            .catch(function (err) {
+                $scope.error = err.message;
+            });
+    };
+
+
+}]);    
+app.controller("loginController", ["$scope", "$firebaseAuth", "$location", function ($scope, $firebaseAuth, $location) {
+    $scope.login = function (user) {
+        $firebaseAuth().$signInWithEmailAndPassword(user.email, user.password)
+        .then(function (user) {
+            $location.path('/');
+        })
+        .catch(function (err) {
+            $scope.error = err.message;
+        })      
+    };
+
+}]);
+app.controller("AuthCtrl", ["$scope", "$location","$firebaseAuth", function ($scope, $location,$firebaseAuth) {
+    $firebaseAuth().$onAuthStateChanged(function(user){ // $onAuthStateChanged visar om iloggad eller outloggad 
+        if(user){
+            $scope.user= user;
+        }else{
+            $scope.user= null;
         }
-      }
-      $('#button').click(function () { translate(); });
-      function translate() {
-        var sourceText = $('#transcript').val();
-        var sourceLang = document.getElementById("chang-språk-2").value
-        var targetLang = document.getElementById("chang-språk-3").value
-        var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+            
+    })
+    $scope.signOut = function () {
+        $firebaseAuth().$signOut();
+        $location.path('/');
+    };
+    app.controller("control1", function ($scope) {
+
+        $scope.startRecording = function () {
+          startRecording();
+        }
+        function startRecording() {
+          if (window.hasOwnProperty('webkitSpeechRecognition')) {
+            var recognition = new webkitSpeechRecognition();
+            recognition.continuous = false; // för att recording stop om 10 sek // om det är 
+            recognition.interimResults = true;// om det är true så det skriva som jag säger 
       
+            recognition.lang = document.getElementById("chang-språk-1").value;// jag gjorde det för att byta språk 
+            recognition.start(); // för att börjar rccording
       
-        $.getJSON(url, function (data) {
-          $('textarea#resultText').val(data[0][0][0]);
+            recognition.onresult = function (e) {
+              document.getElementById('transcript').value
+                = e.results[0][0].transcript;
+            };
+          }
+        }
       
-        });
+        $('#button').click(function () { translate(); });
+        function translate() {
+          var sourceText = $('#transcript').val();
+          var sourceLang = document.getElementById("chang-språk-2").value
+          var targetLang = document.getElementById("chang-språk-3").value
+          var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+          $.getJSON(url, function (data) {
+            $('textarea#resultText').val(data[0][0][0]);
       
-      }
+          });
+        }
+      })
       
-      
-      
-})
-app.controller("control2",function($scope){
-$scope.new="heshko"
-})
+}])
